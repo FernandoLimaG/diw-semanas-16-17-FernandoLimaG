dbd2ff79f7e56534c893a747f275e3d46d00e844
Fernando de Lima Gonçalves <fernando.limag@outlook.com>
2026-06-14 21:02:37 -0300

    Implementa CRUD completo com JSON Server

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -26,11 +26,34 @@ if (form) {
                 throw new Error('Erro ao cadastrar contato');
             }
 
-            alert('Contato cadastrado com sucesso!');
+            const mensagem = document.getElementById('mensagem');
+
+            mensagem.innerHTML = `
+                <div class="alert alert-success">
+                    Contato cadastrado com sucesso!
+                </div>
+            `;
+
             form.reset();
+
+            setTimeout(() => {
+                mensagem.innerHTML = "";
+            }, 3000);
+
         } catch (error) {
             console.error('Erro ao cadastrar contato:', error);
-            alert('Erro ao cadastrar contato. Por favor, tente novamente.');    
+
+            const mensagem = document.getElementById('mensagem');
+
+            mensagem.innerHTML = `
+                <div class="alert alert-success">
+                    Erro ao cadastrar contato.
+                </div>
+            `;
+
+            setTimeout(() => {
+                mensagem.innerHTML = "";
+            }, 3000);
         }
     });
 }
@@ -56,11 +79,79 @@ async function carregarContatos() {
                 <td>${contato.nome}</td>
                 <td>${contato.telefone}</td>
                 <td>${contato.email}</td>
+                <td>
+                    <button class="btn btn-warning btn-sm" onclick="editarContato('${contato.id}')">
+                        Editar
+                    </button>
+
+                    <button class="btn btn-danger btn-sm" onclick="excluirContato('${contato.id}')">
+                        Excluir
+                    </button>
+                </td>
             `;
             tabelaContatos.appendChild(linha);
         });
     } catch (error) {
         console.error('Erro ao carregar contatos:', error);
         alert('Erro ao carregar contatos. Por favor, tente novamente.');
+    }
+}
+
+async function excluirContato(id) {
+    const confirmar = confirm("Deseja excluir este contato?");
+
+    if (!confirmar) {
+        return;
+    }
+
+    try {
+        const resposta = await fetch(`${API_URL}/${id}`, {
+            method: "DELETE"
+        });
+
+        if (!resposta.ok) {
+            throw new Error("Erro ao excluir contato");
+        }
+
+        carregarContatos();
+    } catch (error) {
+        console.error(error);
+        alert("Erro ao excluir contato");
+    }
+    
+}
+
+async function editarContato(id) {
+    const novoNome = prompt("Nome: ");
+    const novoTelefone = prompt("Telefone: ");
+    const novoEmail = prompt("Email: ");
+
+    if (!novoNome || !novoTelefone) {
+        alert("Os campos 'Nome' e 'Telefone' são obrigatórios.");
+    }
+
+    const contatoAtualizado = {
+        nome: novoNome,
+        telefone: novoTelefone,
+        email: novoEmail
+    };
+
+    try {
+        const resposta = await fetch(`${API_URL}/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(contatoAtualizado)
+        });
+
+        if(!resposta.ok) {
+            throw new Error("Erro ao editar o contato");
+        }
+
+        carregarContatos();
+    } catch(error) {
+        console.error(error);
+        alert("Erro ao editar contato.");
     }
 }
